bcf1ef4511d3010486d0326f9bc01188889e7a42
Liang <goodbye388@gmail.com>
2021-03-10 00:19:12 +0800

    Feat: Modify index.hbs with dynamic data

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const restaurantsList = require('../restaurant.json').results
 
 const getIndex = (req, res) => {
-	res.render('index')
+	res.render('index', { restaurants: restaurantsList })
 }
 exports.getIndex = getIndex
