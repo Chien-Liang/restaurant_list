64c7b3a5a12f610449071862a5f641580c066514
Liang <goodbye388@gmail.com>
2021-03-09 23:51:18 +0800

    Feat: Add the public(dir) and static files & Update main.hbs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 // Routers
 app.get('/', (req, res) => {
